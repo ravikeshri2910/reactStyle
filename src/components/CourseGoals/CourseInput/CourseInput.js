941c2534a25b54c619b78ab632bcdeaa4af41e32
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-10-27 23:52:28 +0530

    first commit

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -18,6 +18,7 @@ const CourseInput = props => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false)
+      
       return;
     }
     props.onAddGoal(enteredValue);
